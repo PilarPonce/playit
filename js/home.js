05ac2b0975345222d370f67caa8f7a6aea32d587
justina <jpadulla@udesa.edu.ar>
2019-06-20 20:56:11 -0300

    anda el link

--- js/home.js
+++ js/home.js
@@ -52,7 +52,7 @@ fetch("https://api.themoviedb.org/3/movie/popular?api_key=704bd3935947752adbb2e6
     console.log(peliculas);
 
     for (var i = 0; i < peliculas.length; i++) {
-      document.querySelector(".contenedor-populares").innerHTML += '<li><div class="uk-panel"><a href="detalle.html?idPelicula="+arrayDePeliculas[i].id+""><img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a><div class="uk-position-center uk-panel"><h1></h1></div></div></li>'
+      document.querySelector(".contenedor-populares").innerHTML += '<li><div class="uk-panel"><a href="detalle.html?idPelicula='+peliculas[i].id+'"><img src="https://image.tmdb.org/t/p/original/' + peliculas[i].poster_path + '" alt=""></a><div class="uk-position-center uk-panel"><h1></h1></div></div></li>'
     }
   })
 
@@ -78,7 +78,7 @@ fetch("https://api.themoviedb.org/3/movie/popular?api_key=704bd3935947752adbb2e6
         console.log(peliculas);
 
         for (var i = 0; i < peliculas.length; i++) {
-          document.querySelector(".contenedor-proximamente").innerHTML += '<li><div class="uk-panel"><a href="detalle.html?idPelicula="+arrayDePeliculas[i].id+""><img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a><div class="uk-position-center uk-panel"><h1></h1></div></div></li>'
+          document.querySelector(".contenedor-proximamente").innerHTML += '<li><div class="uk-panel"><a href="detalle.html?idPelicula='+peliculas[i].id+'"><img src="https://image.tmdb.org/t/p/original/' + peliculas[i].poster_path + '" alt=""></a><div class="uk-position-center uk-panel"><h1></h1></div></div></li>'
         }
       })
 
